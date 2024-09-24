ebaae8cff894de376e9627f6d728f098afb8be46
cesar <cesarks81@gmail.com>
2024-09-24 09:54:29 +0200

    Programa actualizado para mostrar mi lugar favorito

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,7 @@
 public class Prueba {
 	public static void main (String[] args){
 	System.out.println("Deporte favorito: Ajedrez");
+	System.out.println("Mi lugar favorito es la costa");
 	System.out.println("Instagram");
 	}
 }
